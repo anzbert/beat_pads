7fc7b54b1864dec2f30384790b3b7b0a6ec9313e
anzbert <anzbert@gmail.com>
2022-03-27 22:40:55 +0800

    fixed AT lines with pitch bend

--- lib/components/pads_pads_grid.dart
+++ lib/components/pads_pads_grid.dart
@@ -1,5 +1,3 @@
-import 'package:beat_pads/components/paint_aftertouch_lines.dart';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,37 +10,30 @@ class VariablePads extends StatelessWidget {
     final List<List<int>> rowsList =
         Provider.of<Settings>(context, listen: true).rowsLists;
 
-    return Stack(
-      children: [
-        Center(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 50.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: rowsList.map((row) {
-                return Expanded(
-                  flex: 1,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: row.map((padNote) {
-                        return Expanded(
-                          flex: 1,
-                          child: BeatPad(
-                            note: padNote,
-                          ),
-                        );
-                      }).toList()),
-                );
-              }).toList(),
-            ),
-          ),
+    return Center(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: rowsList.map((row) {
+            return Expanded(
+              flex: 1,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: row.map((padNote) {
+                    return Expanded(
+                      flex: 1,
+                      child: BeatPad(
+                        note: padNote,
+                      ),
+                    );
+                  }).toList()),
+            );
+          }).toList(),
         ),
-
-        // Display drawn after touch lines on Top:
-        PaintAfterTouchLines(),
-      ],
+      ),
     );
   }
 }
