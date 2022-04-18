52bb978af8018011bb5762560e619e392e8f2c09
anzbert <anzbert@gmail.com>
2022-04-18 16:13:14 +0800

    sustainbutton

--- lib/screen_beat_pads/button_sustain.dart
+++ lib/screen_beat_pads/button_sustain.dart
@@ -1,27 +1,63 @@
-import 'dart:ui';
-
+import 'package:beat_pads/screen_home/model_settings.dart';
 import 'package:beat_pads/shared/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_midi_command/flutter_midi_command_messages.dart';
+import 'package:provider/provider.dart';
 
-class SustainButton extends StatelessWidget {
+class SustainButton extends StatefulWidget {
   const SustainButton({Key? key}) : super(key: key);
 
   @override
+  State<SustainButton> createState() => _SustainButtonState();
+}
+
+class _SustainButtonState extends State<SustainButton> {
+  int? disposeChannel;
+
+  @override
+  void dispose() {
+    if (disposeChannel != null) {
+      CCMessage(
+        channel: disposeChannel!,
+        controller: 64,
+        value: 0,
+      ).send();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        CCMessage().send();
+    int channel = Provider.of<Settings>(context, listen: true).channel;
+    disposeChannel = channel;
+
+    return Listener(
+      onPointerDown: (_) {
+        CCMessage(
+          channel: channel,
+          controller: 64,
+          value: 127,
+        ).send();
       },
-      child: Text(
-        'S',
-        style: TextStyle(fontSize: 30),
-      ),
-      style: ElevatedButton.styleFrom(
-        shape: CircleBorder(),
-        padding: EdgeInsets.all(20),
-        primary: Palette.yellowGreen.color,
-        onPrimary: Palette.darkGrey.color,
+      onPointerUp: (_) {
+        CCMessage(
+          channel: channel,
+          controller: 64,
+          value: 0,
+        ).send();
+      },
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          'S',
+          style: TextStyle(fontSize: 30),
+        ),
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(20),
+          primary: Palette.yellowGreen.color,
+          onPrimary: Palette.darkGrey.color,
+        ),
       ),
     );
   }
