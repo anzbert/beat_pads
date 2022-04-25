b4c6dbcd534b000f85115c8d3d32ea8074014736
anzbert <anzbert@gmail.com>
2022-04-25 17:09:13 +0800

    sustainrtefresh

--- lib/screen_beat_pads/button_sustain.dart
+++ lib/screen_beat_pads/button_sustain.dart
@@ -46,10 +46,12 @@ class _SustainButtonRectState extends State<SustainButtonRect> {
       padding: EdgeInsets.fromLTRB(0, padSize, padSize, padSize),
       child: Listener(
         onPointerDown: (_) {
-          MidiUtils.sustainMessage(channel, true);
-          setState(() {
-            sustainState = true;
-          });
+          if (!sustainState) {
+            MidiUtils.sustainMessage(channel, true);
+            setState(() {
+              sustainState = true;
+            });
+          }
         },
         onPointerUp: (touch) {
           if (!notOnButtonRect(touch.position)) {
