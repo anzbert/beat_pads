5cdcc792ad1fef07194aabbe6f520b5dd1c13a0d
anzbert <anzbert@gmail.com>
2022-04-28 12:54:30 +0800

    cleanup

--- lib/screen_beat_pads/slide_pad.dart
+++ lib/screen_beat_pads/slide_pad.dart
@@ -15,14 +15,9 @@ class SlideBeatPad extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // GET DATA:
-    final int rootNote = Provider.of<Settings>(context, listen: true).rootNote;
-    final List<int> scale =
-        Provider.of<Settings>(context, listen: true).scaleList;
-    final bool showNoteNames =
-        Provider.of<Settings>(context, listen: true).showNoteNames;
+    final Settings settings = Provider.of<Settings>(context, listen: true);
 
-    final int _rxNote = note < 127 && note >= 0
+    final int rxNote = note < 127 && note >= 0
         ? Provider.of<MidiReceiver>(context, listen: true).rxBuffer[note]
         : 0;
 
@@ -30,40 +25,43 @@ class SlideBeatPad extends StatelessWidget {
         Provider.of<MidiSender>(context, listen: true).noteIsOn(note);
 
     // PAD COLOR:
-    final Color _color;
-    final Color _splashColor = Palette.lightPink.color;
-    final Color _padTextColor = Palette.darkGrey.color;
+    final Color splashColor = Palette.lightPink.color;
+    final Color padTextColor = Palette.darkGrey.color;
 
+    final Color color;
     if (noteOn) {
-      _color = _splashColor.withAlpha(220); // maintain color when pushed
-    } else if (_rxNote > 0) {
-      _color = Palette.cadetBlue.color.withAlpha(
-          _rxNote * 2); // receiving midi signal adjusted by received velocity
+      color = splashColor.withAlpha(220); // maintain color when pushed
+
+    } else if (rxNote > 0) {
+      color = Palette.cadetBlue.color.withAlpha(
+          rxNote * 2); // receiving midi signal adjusted by received velocity
+
     } else if (note > 127 || note < 0) {
-      _color = Palette.darkGrey.color; // out of midi range
+      color = Palette.darkGrey.color; // out of midi range
 
-    } else if (!MidiUtils.isNoteInScale(note, scale, rootNote)) {
-      _color = Palette.yellowGreen.color.withAlpha(160); // not in current scale
+    } else if (!MidiUtils.isNoteInScale(
+        note, settings.scaleList, settings.rootNote)) {
+      color = Palette.yellowGreen.color.withAlpha(160); // not in current scale
 
-    } else if (note % 12 == rootNote) {
-      _color = Palette.laserLemon.color; // root note
+    } else if (note % 12 == settings.rootNote) {
+      color = Palette.laserLemon.color; // root note
 
     } else {
-      _color = Palette.yellowGreen.color; // default pad color
+      color = Palette.yellowGreen.color; // default pad color
     }
 
-    Size size = MediaQuery.of(context).size;
-    double _fontSize = size.width * 0.022;
-    BorderRadius _padRadius =
-        BorderRadius.all(Radius.circular(size.width * 0.008));
+    double screenWidth = MediaQuery.of(context).size.width;
+    double fontSize = screenWidth * 0.022;
+    BorderRadius padRadius =
+        BorderRadius.all(Radius.circular(screenWidth * 0.008));
 
     return Container(
-      padding: EdgeInsets.all(size.width * 0.005),
+      padding: EdgeInsets.all(screenWidth * 0.005),
       height: double.infinity,
       width: double.infinity,
       child: Material(
-        color: _color,
-        borderRadius: _padRadius,
+        color: color,
+        borderRadius: padRadius,
         elevation: 5,
         shadowColor: Colors.black,
         child: note > 127 || note < 0
@@ -71,7 +69,7 @@ class SlideBeatPad extends StatelessWidget {
             // OUT OF MIDI RANGE
             InkWell(
                 onTapDown: (_) {},
-                borderRadius: _padRadius,
+                borderRadius: padRadius,
                 child: Padding(
                   padding: _padPadding,
                   child: Text(
@@ -79,7 +77,7 @@ class SlideBeatPad extends StatelessWidget {
                     style: TextStyle(
                       fontStyle: FontStyle.italic,
                       color: Colors.grey,
-                      fontSize: _fontSize * 0.8,
+                      fontSize: fontSize * 0.8,
                     ),
                   ),
                 ),
@@ -88,17 +86,17 @@ class SlideBeatPad extends StatelessWidget {
             // WITHIN MIDI RANGE
             InkWell(
                 onTapDown: (_) {},
-                borderRadius: _padRadius,
-                splashColor: _splashColor,
+                borderRadius: padRadius,
+                splashColor: splashColor,
                 child: Padding(
                   padding: _padPadding,
                   child: Text(
-                      showNoteNames
+                      settings.showNoteNames
                           ? MidiUtils.getNoteName(note, showNoteValue: false)
                           : note.toString(),
                       style: TextStyle(
-                        color: _padTextColor,
-                        fontSize: _fontSize,
+                        color: padTextColor,
+                        fontSize: fontSize,
                       )),
                 ),
               ),
