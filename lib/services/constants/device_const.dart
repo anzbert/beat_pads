88d06c43ec6b62cb9185ad35d1a56bd3ea2ec13f
anzbert <anzbert@gmail.com>
2022-05-16 20:05:19 +0800

    fix grid sizes

--- lib/services/constants/device_const.dart
+++ lib/services/constants/device_const.dart
@@ -1,11 +1,12 @@
 // Sizes from https://docs.flutter.dev/development/ui/layout/building-adaptive-apps
+// and https://www.ios-resolution.com/
 
 import 'package:flutter/material.dart';
 
 enum ScreenSize {
   small(8),
-  normal(12),
-  large(16);
+  normal(10),
+  large(12);
 
   final int maxGrid;
   const ScreenSize(this.maxGrid);
@@ -13,7 +14,7 @@ enum ScreenSize {
   static ScreenSize getSize(BuildContext context) {
     double deviceWidth = MediaQuery.of(context).size.shortestSide;
     if (deviceWidth > 800) return ScreenSize.large;
-    if (deviceWidth > 550) return ScreenSize.normal;
+    if (deviceWidth > 600) return ScreenSize.normal;
     return ScreenSize.small;
   }
 }
