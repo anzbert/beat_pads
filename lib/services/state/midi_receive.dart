3839444e19d5440b322b847e877a47b868d39ba9
anzbert <anzbert@gmail.com>
2023-03-19 15:25:55 +0800

    remove deprecated riverpod .stream method

--- lib/services/state/midi_receive.dart
+++ lib/services/state/midi_receive.dart
@@ -22,30 +22,6 @@ final rxMidiStream = StreamProvider<MidiMessagePacket>(
   }),
 );
 
-// final rxNoteProvider = NotifierProvider<RxNoteStateNotifier, List<int>>((ref) {
-//   // Listen to the stream itself instead of the value hold by the provider
-//   final midiStream = ref.watch(rxMidiStream.stream);
-
-//   // Create the instance of your StateNotifier
-//   final rxNoteProvider = RxNoteStateNotifier();
-
-//   /// Subscribe to the stream to change the state accordingly
-//   final subscription = midiStream.listen((message) {
-//     if (message.content.length < 2) return;
-
-//     if (message.type == MidiMessageType.noteOn) {
-//       rxNoteProvider.changeValue(message.content[0], message.content[1]);
-//     }
-//     if (message.type == MidiMessageType.noteOff) {
-//       rxNoteProvider.changeValue(message.content[0], 0);
-//     }
-//   });
-
-//   ref.onDispose(subscription.cancel); // Dispose to avoid memory leaks
-
-//   return rxNoteProvider;
-// });
-
 final rxNoteProvider = NotifierProvider<NoteListNotifier, List<int>>(() {
   return NoteListNotifier();
 });
@@ -55,11 +31,8 @@ class NoteListNotifier extends Notifier<List<int>> {
   List<int> build() {
     ref.listen(rxMidiStream,
         (AsyncValue<MidiMessagePacket>? _, AsyncValue<MidiMessagePacket> next) {
-      // GUARDS:
       if (next.hasError || next.hasValue == false || next.value == null) return;
       if (next.value!.content.length < 2) return; // message too short
-      if (next.value!.type != MidiMessageType.noteOn ||
-          next.value!.type != MidiMessageType.noteOff) return; // not a note msg
 
       if (next.value!.type == MidiMessageType.noteOn) {
         changeValue(next.value!.content[0], next.value!.content[1]);
@@ -79,7 +52,7 @@ class NoteListNotifier extends Notifier<List<int>> {
     }
   }
 
-  void reset() {
-    state = List.filled(128, 0, growable: false);
-  }
+  // void reset() {
+  //   state = List.filled(128, 0, growable: false);
+  // }
 }
