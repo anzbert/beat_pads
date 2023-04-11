5262a38f37721375beae91f84375ceb1fc327b41
anzbert <anzbert@gmail.com>
2023-04-11 11:48:41 +0800

    mpe next

--- lib/services/playmodes/playhandler_mpe.dart
+++ lib/services/playmodes/playhandler_mpe.dart
@@ -1,18 +1,17 @@
 import 'package:beat_pads/services/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PlayModeMPE extends PlayModeHandler {
   final SendMpe mpeMods;
   final MemberChannelProvider channelProvider;
-  final ProviderRef<PlayModeHandler> ref;
 
-  PlayModeMPE(this.ref): mpeMods = SendMpe(
-          settings.mpe2DX.getMod(settings.mpePitchbendRange),
-          settings.mpe2DY.getMod(settings.mpePitchbendRange),
-          settings.mpe1DRadius.getMod(settings.mpePitchbendRange),
+  PlayModeMPE(super.ref)
+      : mpeMods = SendMpe(
+          ref.read(mpe2DXProv).getMod(ref.read(mpePitchbendRangeProv)),
+          ref.read(mpe2DYProv).getMod(ref.read(mpePitchbendRangeProv)),
+          ref.read(mpe1DRadiusProv).getMod(ref.read(mpePitchbendRangeProv)),
         ),
         channelProvider =
-            MemberChannelProvider(settings.zone, settings.mpeMemberChannels);;
+            MemberChannelProvider(settings.zone, settings.mpeMemberChannels);
 
   /// Release channel in MPE channel provider
   @override
@@ -23,42 +22,47 @@ class PlayModeMPE extends PlayModeHandler {
   @override
   void handleNewTouch(PadTouchAndScreenData data) {
     // remove note if it is still playing
-    if (settings.modReleaseTime > 0 || settings.noteReleaseTime > 0) {
-      touchReleaseBuffer.removeNoteFromReleaseBuffer(data.padNote);
+    if (ref.read(modReleaseUsable) > 0 || ref.read(noteReleaseUsable) > 0) {
+      ref
+          .read(noteReleaseBuffer.notifier)
+          .removeNoteFromReleaseBuffer(data.padNote);
     }
 
     int newChannel = channelProvider.provideChannel([
-      ...touchBuffer.buffer,
-      ...touchReleaseBuffer.buffer
+      ...ref.read(touchBuffer),
+      ...ref.read(touchReleaseBuffer)
     ]); // get new channel from generator
 
-    if (settings.modulation2D) {
+    if (ref.read(modulation2DProv)) {
       mpeMods.xMod.send(newChannel, data.padNote, 0);
       mpeMods.yMod.send(newChannel, data.padNote, 0);
     } else {
       mpeMods.rMod.send(newChannel, data.padNote, 0);
     }
 
-    NoteEvent noteOn = NoteEvent(
-        newChannel, data.padNote, velocityProvider.velocity(data.yPercentage))
+    NoteEvent noteOn = NoteEvent(newChannel, data.padNote,
+        ref.read(velocitySliderValueProv.notifier).velocity(data.yPercentage))
       ..noteOn(cc: false);
 
-    touchBuffer.addNoteOn(CustomPointer(data.pointer, data.screenTouchPos),
-        noteOn, data.screenSize);
-    notifyParent();
+    ref.read(touchBuffer.notifier).addNoteOn(
+        CustomPointer(data.pointer, data.screenTouchPos),
+        noteOn,
+        data.screenSize);
+    // notifyParent();
   }
 
   @override
   // void handlePan(CustomPointer touch, int? note) {
   void handlePan(NullableTouchAndScreenData data) {
-    TouchEvent? eventInBuffer = touchBuffer.getByID(data.pointer) ??
-        touchReleaseBuffer.getByID(data.pointer);
+    TouchEvent? eventInBuffer =
+        ref.read(touchBuffer.notifier).getByID(data.pointer) ??
+            ref.read(touchReleaseBuffer.notifier).getByID(data.pointer);
     if (eventInBuffer == null) return;
 
     eventInBuffer.updatePosition(data.screenTouchPos);
-    notifyParent(); // for circle drawing
+    // notifyParent(); // for circle drawing
 
-    if (settings.modulation2D) {
+    if (ref.read(modulation2DProv)) {
       mpeMods.xMod.send(
         eventInBuffer.noteEvent.channel,
         eventInBuffer.noteEvent.note,
