62ab35cfb7182c89fb0e1e67653484d501770adc
anzbert <anzbert@gmail.com>
2024-03-25 08:04:09 +0800

    refactor playmode enum

--- lib/services/playmodes/enum_playmode.dart
+++ lib/services/playmodes/enum_playmode.dart
@@ -3,48 +3,49 @@ import 'package:beat_pads/services/services.dart';
 // TODO Add targeted Pitch slide MPE mode like on the Push 3
 
 enum PlayMode {
-  slide('Trigger Notes'),
   noSlide('Disabled'),
-  polyAT('Poly Aftertouch'),
-  mpe('MPE'),
-  mpeTargetPb('MPE - Pitch to Pad');
+  slide('Trigger Notes'),
 
-  const PlayMode(this.title);
+  polyAT(
+    'Poly Aftertouch',
+    modulatable: true,
+  ),
+
+  mpe(
+    'MPE',
+    modulatable: true,
+    oneDimensional: true,
+    singleChannel: false,
+  ),
+
+  mpeTargetPb(
+    'MPE - Pitch to Pad',
+    modulatable: true,
+    oneDimensional: true,
+    singleChannel: false,
+  );
+
+  const PlayMode(
+    this.title, {
+    this.modulatable = false,
+    this.oneDimensional = true,
+    this.singleChannel = true,
+  });
+
   final String title;
+
+  /// Layout uses a modulation overlay and can be modulated by finger panning
+  final bool modulatable;
+
+  /// Layout uses a modulation overlay that is circular with only
+  /// one modulated parameter
+  final bool oneDimensional;
+
+  /// Layout sends Midi on one channel only (Usually all Non-MPE Layouts)
+  final bool singleChannel;
 
   @override
   String toString() => title;
-
-  bool get modulatable {
-    switch (this) {
-      case PlayMode.polyAT:
-        return true;
-      case PlayMode.mpe:
-        return true;
-      case PlayMode.mpeTargetPb:
-        return true;
-      // ignore: no_default_cases
-      default:
-        return false;
-    }
-  }
-
-  bool get oneDimensional {
-    switch (this) {
-      case PlayMode.mpe:
-        return false;
-      case PlayMode.mpeTargetPb:
-        return false;
-      // ignore: no_default_cases
-      default:
-        return true;
-    }
-  }
-
-  bool get singleChannel {
-    if (this == PlayMode.mpe || this == PlayMode.mpeTargetPb) return false;
-    return true;
-  }
 
   PlayModeHandler getPlayModeApi(
     SendSettings settings,
