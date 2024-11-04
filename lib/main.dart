17785e4109899704b418a384b61789853eeea66c
anzbert <anzbert@gmail.com>
2024-11-04 12:05:57 +0800

    perf improv

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:beat_pads/screen_splash/_screen_splash.dart';
 import 'package:beat_pads/services/services.dart';
 import 'package:beat_pads/theme.dart';
 // ignore: unused_import
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // ignore: unused_import
-import 'package:flutter/rendering.dart'; // for debug repaint rainbow, if enabled in main()
+// import 'package:flutter/rendering.dart'; // for debug repaint rainbow, if enabled in main()
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // SHARED PREFERENCES PROVIDER //////////////////////////////////////////
